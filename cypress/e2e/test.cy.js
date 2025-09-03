e992cf4b321657af8a75a63a34d8c9c84e7b7064
Miika Takila <miika.takila@gmail.com>
2025-09-03 12:05:14 +0300

    broke test intentionally to see if i get failure message on discord

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -7,6 +7,6 @@ describe('Pokedex', function() {
   it('pokemon page can be navigated to', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur').click()
-    cy.contains('chlorophyll')
+    cy.contains('Chlorophyll')
   })
 })
